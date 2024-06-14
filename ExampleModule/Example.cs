64bc498b4e61518bcf2287424e76e3ce6c9b81aa
bob <bob@DESKTOP-8IV5CKE>
2024-06-14 17:30:25 +0200

    asfd

--- ExampleModule/Example.cs
+++ ExampleModule/Example.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BepInEx.Configuration;
+using Displyy.Utils;
+using System;
+using UnityEngine.EventSystems;
 public class ExternalModule : BaseModule
 {
     public override bool Enabled { get; set; } = false;
@@ -21,4 +24,37 @@ public class ExternalModule : BaseModule
     {
         Console.WriteLine("External OnDisable");
     }
+
+    /*Config stuff
+     * example: 
+    */
+    //private static float howfast;
+    public override void ReloadConfiguration()
+    {
+        //if user changes the config, this will run. for example:
+        //howfast = CEhowfast.Value * 1.5f;
+    }
+/*    private static ConfigEntry<float> CEhowfast;
+    private static ConfigEntry<int> CEbuttonToDo;
+*/
+    public static void BindConfigEntries(string DisplayName)
+    {
+/*        SettingsHelper settingsHelper = new SettingsHelper();
+        CEhowfast = settingsHelper.GetConfigFile().Bind<float>(
+        section: DisplayName,
+           key: "Speed",
+           defaultValue: 15f,
+           new ConfigDescription(
+           "How fast you fly",
+           new AcceptableValueRange<float>(1f, 50f)
+        ));
+        // What button to make the bool shouldRun true.
+        CEbuttonToDo = settingsHelper.GetConfigFile().Bind(
+        section: DisplayName,
+        key: "Controller button", // has to be named Controller Button
+            defaultValue: (int)EasyInput.ControllerButton.bButton,
+            description: "What button to fly"
+        );*/
+    }
+
 }
