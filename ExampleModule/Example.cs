a531d0a5969b0093774047feef3c69f2dd8b1e7b
bob <bob@DESKTOP-8IV5CKE>
2024-06-14 16:39:51 +0200

    Add project files.

---
+++ ExampleModule/Example.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class ExternalModule : BaseModule
+{
+    public override bool Enabled { get; set; } = false;
+    public override string Category { get; set; } = "External";
+    public override string DisplayName { get; set; } = "External Module";
+
+    public override string GetTooltip()
+    {
+        return "This is an external module.";
+    }
+    private void OnEnable()
+    {
+        Console.WriteLine("External OnEnable");
+    }
+    private void Update()
+    {
+        Console.WriteLine("External Update");
+    }
+    private void OnDisable()
+    {
+        Console.WriteLine("External OnDisable");
+    }
+}
